8519a1f822fa209d5728e401263f9c1dd94b7ed9
Lukas <lukasstancikas@gmail.com>
2018-06-05 16:57:09 +0300

    data classes to interfaces

--- noteboard-model/src/main/java/stancikas/lukas/noteboard/Note.kt
+++ noteboard-model/src/main/java/stancikas/lukas/noteboard/Note.kt
@@ -1,3 +1,9 @@
 package stancikas.lukas.noteboard
 
-data class Note(val id: Long, val text: String, val isDone: Boolean, val dateCreated: Long, val dateModified: Long)
+interface Note {
+	val id: Long
+	val text: String
+	val isDone: Boolean
+	val dateCreated: Long
+	val dateModified: Long
+}
--- noteboard-model/src/main/java/stancikas/lukas/noteboard/Page.kt
+++ noteboard-model/src/main/java/stancikas/lukas/noteboard/Page.kt
@@ -1,3 +1,9 @@
 package stancikas.lukas.noteboard
 
-data class Page(val id: Long, val title: String, val items: List<Note>, val dateCreated: Long, val dateModified: Long)
+interface Page {
+	val id: Long
+	val title: String
+	val items: List<Note>
+	val dateCreated: Long
+	val dateModified: Long
+}
